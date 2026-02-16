ae4ddaa151aaa33e68912d8ab79ca13ebe89098a
smaran169 <barlasmaran4@gmail.com>
2026-02-16 15:06:49 +0530

    ,es

--- IV-SEM/Python/Assigments/M01_Language_&_Programming_Basics/AST03/task.py
+++ IV-SEM/Python/Assigments/M01_Language_&_Programming_Basics/AST03/task.py
@@ -1,15 +1,18 @@
+import math
+
 def Student_Grade_System(name: str, n1: int, n2: int, n3: int) -> str:
-    # Calculate average
-    average = (n1 + n2 + n3) / 3
-
-    # Determine status
-    status = "Pass" if average >= 40 else "Fail"
-
-    # Format output
-    return f"Average grade: {average:.2f}, Status: {status}"
+    
+    avg = (n1 + n2 + n3) / 3
+    
+    avg_truncated = math.floor(avg * 100) / 100
+    
+    avg_str = f"{avg_truncated:.1f}" if avg_truncated.is_integer() else f"{avg_truncated:.2f}"
+    
+    status = "Pass" if avg_truncated >= 40 else "fail"
+    return f"Average grade: {avg_str}, Status: {status}"
 
 
 if __name__ == '__main__':
     name = input().strip()
-    n1, n2, n3 = list(map(int, input().split()))
+    n1, n2, n3 = map(int, input().split())
     print(Student_Grade_System(name, n1, n2, n3))
